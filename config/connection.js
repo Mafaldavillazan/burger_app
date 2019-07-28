dbdde789d55df10b173ad386bf85725deba91d84
Mafalda <mafalda.villazan@gmail.com>
2019-07-28 14:45:07 -0700

    Adding ignore field to hide credentials for the data base

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+//---------------------------------
+//          CONNECTION TO DB
+//---------------------------------
+
+//Requiring mysql
+var mysql = require("mysql");
+
+//Connection credentials, with security field
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA
+});
+
+// Make a connection (if not show problem)
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection
+module.exports = connection;
